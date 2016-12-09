f3e4d1f87367eefc30a31eede491249ae02d1b35
nzkks <kkshanthosh@gmail.com>
2016-12-09 21:51:08 +1300

    my first chai mocha test

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('A simple mocho chai test', function(){
+	it('should pass when everything is okay', function(){
+		expect(true).to.be.true;
+	});
+});
